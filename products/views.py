bcb1364a0bc2463e50e17c6fbc7fe3a7ced759e2
farah1979 <farahhisham1979@gmail.com>
2022-01-05 00:23:32 +0000

    started products template

---
+++ products/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Product
+
+
+def all_products(request):
+    """ A view to show all products """
+
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
