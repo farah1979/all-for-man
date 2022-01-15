5492a9b91bfb5acead0bbaf28effdb39f71955ec
farah1979 <farahhisham1979@gmail.com>
2022-01-15 14:28:30 +0000

    updated the stripe elements js and corrected the webhooks secret key

--- checkout/forms.py
+++ checkout/forms.py
@@ -30,6 +30,7 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
+            print(field)
             if field != 'country':
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *'
