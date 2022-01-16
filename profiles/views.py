c5a1d96eb948a389eedf800e607c83879cf068a0
farah1979 <farahhisham1979@gmail.com>
2022-01-16 21:37:08 +0000

    updated webhooks handler to handle profiles if the form failed

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
@@ -13,8 +14,11 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully!')
+        else:
+            messages.error(request, 'Please check that form is valid')
 
-    form = UserProfileForm(instance=profile)
+    else:
+        form = UserProfileForm(request.POST, instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -24,4 +28,18 @@ def profile(request):
         'on_profile_page': True,
     }
 
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+            f'This is a past confirmation for order number {order_number}.'
+               'A confirmation email was sent on the order date.'))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
     return render(request, template, context)
