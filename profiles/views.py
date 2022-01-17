f19238f2cc17bf51330301eef3acbe81a9b8ce69
farah1979 <farahhisham1979@gmail.com>
2022-01-17 23:11:54 +0000

    Product Admin Securing the views

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
+from django.contrib.auth.decorators import login_required
 from .forms import UserProfileForm
 from checkout.models import Order
 
 
+
+@login_required
 def profile(request):
     """ Display the user's profile. """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry only store owners can do that.')
+        return redirect(reverse('home'))
+
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -30,7 +37,13 @@ def profile(request):
 
     return render(request, template, context)
 
+
+@login_required
 def order_history(request, order_number):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry only store owners can do that.')
+        return redirect(reverse('home'))
+
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
