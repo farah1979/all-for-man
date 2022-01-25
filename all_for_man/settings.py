126d0e8db6e71bc44240cf233ae32d93f4d3b55d
farah1979 <farahhisham1979@gmail.com>
2022-01-25 20:50:29 +0000

    removed secret key and set debug

--- all_for_man/settings.py
+++ all_for_man/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+o&vy2mxe3!fs=0+17@ft5$7aom40&5cc5lr1v7e5+80ndw=79'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['all-for-man.herokuapp.com','localhost']
 
